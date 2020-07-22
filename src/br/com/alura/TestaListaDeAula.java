60041ba3cfa2c470c5a13eae2c669d7071ede895
Arthur da Silva Nunes <arthurnunes@brq.com>
2020-07-22 17:07:42 -0300

    Estudando Collections

---
+++ src/br/com/alura/TestaListaDeAula.java
@@ -0,0 +1,33 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestaListaDeAula {
+
+	public static void main(String[] args) {
+		// Variáveis
+		Aula a1 = new Aula("Revistando as ArrayLists", 21);
+		Aula a2 = new Aula("Listas de objetos", 20);
+		Aula a3 = new Aula("Relacionamento de listas e obetos", 15);
+		// Lista
+		ArrayList<Aula> aulas = new ArrayList<>();
+		aulas.add(a1);
+		aulas.add(a2);
+		aulas.add(a3);
+		System.out.println(aulas);
+		
+		// Ordena uma lista do tipo Aula
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		
+		// Ordena uma lista do tipo Aula ordenada pelo tempo
+		Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+		System.out.println(aulas);
+		
+		
+
+	}
+
+}
